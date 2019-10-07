8b49c8a3c9738b41ceb1ae9798e85578357b8cc5
Unknown <fajar7xx@gmail.com>
2019-10-07 19:17:26 +0700

    ubah config base url index php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,8 @@ $config['base_url'] = 'http://localhost/belajarci/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
